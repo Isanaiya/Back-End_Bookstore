7c54e04471605c2ccd8d920f43947e72eac4e28e
Joona Virolainen <91015110+Isanaiya@users.noreply.github.com>
2023-09-09 07:59:30 +0300

    Add everything current.

---
+++ src/main/java/com/example/Bookstore/BookController.java
@@ -0,0 +1,14 @@
+package com.example.Bookstore;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class BookController {
+	
+//	@RequestMapping("/index")
+//	public String bookList(Model model) {
+//		model.addAttribute("books", repository.findAll());
+//	}
+}
