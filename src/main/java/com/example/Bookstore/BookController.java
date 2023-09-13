b6608296a057ded9373d592961b6ad4d97861a39
Joona Virolainen <91015110+Isanaiya@users.noreply.github.com>
2023-09-14 02:42:17 +0300

    Bookstore task - Chapter 3 2/4

--- src/main/java/com/example/Bookstore/BookController.java
+++ src/main/java/com/example/Bookstore/BookController.java
@@ -1,14 +1,21 @@
 package com.example.Bookstore;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.Bookstore.domain.BookRepository;
+
 @Controller
 public class BookController {
 	
-//	@RequestMapping("/index")
-//	public String bookList(Model model) {
-//		model.addAttribute("books", repository.findAll());
-//	}
+	@Autowired
+	private BookRepository repository;
+	
+	@RequestMapping(value={"/" , "/booklist"})
+	public String bookList(Model model) {
+		model.addAttribute("books", repository.findAll());
+		return "booklist";
+	}
 }
