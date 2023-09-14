bdbfe279295c896f9990d94b667eac376473e443
Joona Virolainen <91015110+Isanaiya@users.noreply.github.com>
2023-09-14 06:17:02 +0300

    Bookstore task - Chapter 3 4/4

--- src/main/java/com/example/Bookstore/BookController.java
+++ src/main/java/com/example/Bookstore/BookController.java
@@ -1,5 +1,7 @@
 package com.example.Bookstore;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -27,6 +29,19 @@ public class BookController {
 	public String addBook(Model model) {
 		model.addAttribute("book", new Book());
 		return "addbook";
+	}
+	
+	@RequestMapping(value = "/edit/{id}", method = RequestMethod.GET)
+	public String editBook(@PathVariable("id") Long bookId, Model model) {
+		Optional<Book> book = repository.findById(bookId);
+		model.addAttribute("book", book);
+		return "editbook";
+	}
+	
+	@PostMapping(value = "/edit/save")
+	public String saveEdit(Book book) {
+		repository.save(book);
+		return "redirect:/booklist";
 	}
 	
 	@PostMapping(value = "/save")
